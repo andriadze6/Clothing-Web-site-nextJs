597c7fd3c7b4e7d5d2569ec746205fe1423e3ec7
andriadze66 <giorgiandriadze814@gmail.com>
2024-10-15 13:15:29 +0400

    Change product layout desain

--- app/components/ShopLayout.jsx
+++ app/components/ShopLayout.jsx
@@ -1,7 +1,11 @@
 'Use client'
 import '../assets/css/shopLayout.css'
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartOutline } from '@fortawesome/free-regular-svg-icons';
 import { useEffect, useState } from 'react';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 
 function ShopLayout(){
     const [product, setProduct] = useState([]);
@@ -59,8 +63,25 @@ function ShopLayout(){
                         fProduct.map((element)=>{
                             return(
                                 <div className='card' key={element.id}>
-                                    <Image alt='' src={element.images[0]} width={200} height={200}/>
-                                    <p>{element.title}</p>
+                                    <div className='cardImg'>
+                                        <Image alt='' src={element.images[0]} width={268} height={400}/>
+                                    </div>
+                                    <div className='product_content-header'>
+                                        <h4>{element.title}</h4>
+                                        <FontAwesomeIcon icon={faHeartOutline} className="heart-icon" />
+                                    </div>
+                                    <div className='product_price_rating'>
+                                        <h4><span>$</span>{element.price}</h4>
+                                        <div className='product_rating'>
+                                            <ul>
+                                                <li><FontAwesomeIcon icon={faStar} className="star-icon" /></li>
+                                                <li><FontAwesomeIcon icon={faStar} className="star-icon" /></li>
+                                                <li><FontAwesomeIcon icon={faStar} className="star-icon" /></li>
+                                                <li><FontAwesomeIcon icon={faStar} className="star-icon" /></li>
+                                                <li><FontAwesomeIcon icon={faStar} className="star-icon" /></li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             )
                         })
